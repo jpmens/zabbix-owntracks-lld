4ce45390fece87ccccc81ce96503f05b4b75b424
Jan-Piet Mens <jp@mens.de>
2019-03-28 11:18:37 +0100

    simulator done

--- backend/publish-simulator.py
+++ backend/publish-simulator.py
@@ -11,11 +11,11 @@ import os
 Loc = namedtuple('Loc', 'imei lat lon alt vel cog plate tid')
 
 positions = [
-    Loc("012549656802107", 59.373399,  17.946690, 101,  15, 280, "25-33-XQ", "xq"),
+    Loc("516081298750081", 59.373399,  17.946690, 101,  15, 280, "25-33-XQ", "xq"),
     Loc("507957223448860", 32.851537, -80.012624,  90, 119, 110, "72-93-FQ", "fq"),
     Loc("523674523738680", 41.474304,  15.397448,  14,   0, 110, "JL-19-74", "JL"),
     Loc("867260028922633", 40.878464,  -5.562181, 341,  56, 135, "A 0815 BL", "Gn"),
-    Loc("516081298750081", 48.856826,   2.292713,  43,  72, 205, "20-ER-20", "JJ"),
+    Loc("012549656802107", 48.856826,   2.292713,  43,  72, 205, "20-ER-20", "JJ"),
 ]
 
 params = {
@@ -30,6 +30,9 @@ for p in positions:
     topic = "owntracks/zbx/%s" % (p.imei)
     # adjust random time: five minutes ago plus a bit
     tst = int(time.time()) - 300 + random.randint(0, 120)
+    tst = int(time.time())
+    vel = p.vel
+    # vel = 10
     data = {
         "_type"     : "location",
         "tst"       : tst,
@@ -37,7 +40,7 @@ for p in positions:
         "lon"       : p.lon,
         "cog"       : p.cog,
         "alt"       : p.alt,
-        "vel"       : p.vel,
+        "vel"       : vel,
         "tid"       : p.tid,
         "batt"      : random.randint(82, 100),
         "name"     : p.plate,                   # OSM popup on last/
